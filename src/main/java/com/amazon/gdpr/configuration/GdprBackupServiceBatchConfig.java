1433007eebc23a422e9f8b005fde1e08a8cf3d8b
Apparao Dasari <dapparao@amazon.com>
2021-02-07 11:54:30 +0530

    commit

--- src/main/java/com/amazon/gdpr/configuration/GdprBackupServiceBatchConfig.java
+++ src/main/java/com/amazon/gdpr/configuration/GdprBackupServiceBatchConfig.java
@@ -255,7 +255,7 @@ public class GdprBackupServiceBatchConfig {
 						selectColumns, runId);
 				completeQuery = completeQuery.replaceAll("TAG.", "SF_ARCHIVE.");
 				@SuppressWarnings("unchecked")
-				String backupDataInsertQuery = "INSERT INTO GDPR.AS" + backupTableName + " (ID," + selectColumns + ") "
+				String backupDataInsertQuery = "INSERT INTO GDPR." + backupTableName + " (ID," + selectColumns + ") "
 						+ completeQuery + " ON CONFLICT (id) DO UPDATE " + "  SET " + splittedValues + ";";
 				insertcount = backupServiceDaoImpl.insertBackupTable(backupDataInsertQuery);
 
